fa8e32a12c47d30870290f34402180b0e8b6dfeb
varun goel <varun.varungoel.goel@gmail.com>
2020-02-07 17:20:30 +0530

    nodejs intro

---
+++ webminar10/nodejs/lib1.js
@@ -0,0 +1,6 @@
+function awesomefunction () {
+    console.log("OMG! this is great");
+}
+module.export = {         // used for exporting the function to another file .
+    awesomefunction
+}
---
+++ webminar10/nodejs/script1.js
@@ -0,0 +1,3 @@
+const lib = require('./lib.js') // thats for importing the function from other file
+console.log (lib)
+lib.awesomefunction()    // calling of function to perform the task
