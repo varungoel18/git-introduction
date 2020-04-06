a410ecae9d49157d25672c5692292f4f8877c3ac
varun goel <varun.varungoel.goel@gmail.com>
2020-04-06 10:40:25 +0530

    mysql with jquery and api

---
+++ webminar13/mysql_express_server/server1.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.set("view engine","hbs")
+app.use('/pages',require('./routes/pages').route)
+app.use('/api',require('./routes/api').route)
+app.use('/',express.static(path.join(__dirname,'publin_static')))
+
+app.listen(4444, () => {
+console.log("server started at http://localhost:4444/");
+})
